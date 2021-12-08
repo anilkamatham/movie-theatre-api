8ba40b40aefaf5b2506cf366f3d0d7e788c47b54
anilkamatham <75293771+anilkamatham@users.noreply.github.com>
2021-12-08 15:26:31 +0530

    Delete src directory

--- src/main/java/com/bookmymovie/movie/theatre/repository/ScreenDao.java
+++
@@ -1,9 +0,0 @@
-package com.bookmymovie.movie.theatre.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bookmymovie.movie.theatre.model.Screen;
-
-public interface ScreenDao extends JpaRepository<Screen, Integer>{
-
-}
--- src/main/java/com/bookmymovie/movie/theatre/service/CityService.java
+++
@@ -1,8 +0,0 @@
-package com.bookmymovie.movie.theatre.service;
-
-import com.bookmymovie.movie.theatre.model.City;
-
-public interface CityService {
-
-	public City addCity(City city);
-}
